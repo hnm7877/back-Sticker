f3a4b16078bbd0fb05e91a7168925a23705a3d53
LITIE <133286746+hnm7877@users.noreply.github.com>
2024-10-04 16:24:10 +0200

    push main

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AndyVESPUCE\my-watermark-api\venv
+set VIRTUAL_ENV=C:\Users\SouleyLITIE\Documents\GitHub\back-Sticker\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
